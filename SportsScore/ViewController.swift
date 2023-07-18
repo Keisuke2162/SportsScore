1ade4c03c1acf5f854cc9f2b1e944e990ee25127
Keisuke2162 <applabo.kei@gmail.com>
2023-07-18 22:58:05 +0900

    Initial Commit

---
+++ SportsScore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SportsScore
+//
+//  Created by 植田圭祐 on 2023/07/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SportsScoreUITests/SportsScoreUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SportsScoreUITestsLaunchTests.swift
+//  SportsScoreUITests
+//
+//  Created by 植田圭祐 on 2023/07/18.
+//
+
+import XCTest
+
+final class SportsScoreUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
